ebae92ab601342a866c246fcf23a510ed3a318e4
Tom <tom.petri7@gmail.com>
2023-10-21 03:10:18 +0200

    Added Form Validation to Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -9,5 +10,23 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
+  emailIsNotValid: boolean = false;
+  passwordIsNotValid: boolean = false;
 
+
+  logInForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/), Validators.maxLength(64)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(10)]),
+  });
+
+
+  onSubmitForm() {
+    if (this.logInForm.valid) {
+      console.log(this.logInForm.value);
+    } else {
+      this.emailIsNotValid = true;
+      this.passwordIsNotValid = true;
+      console.log('Fail blyyat');
+    }
+  }
 }
