30c0d4a5a4929fde66f1cf1deaaa3d8c769517bf
Tom <tom.petri7@gmail.com>
2023-11-11 02:43:19 +0100

    Added popup with Animation that displayed, if user have successfully signup

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, updateProfile } from "@angular/fire/auth";
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -9,26 +10,33 @@ import { Auth, createUserWithEmailAndPassword, updateProfile } from "@angular/fi
 
 export class AuthService {
   sendingData: boolean = false;
+  signupSuccessfully: boolean = false;
 
 
-  constructor(public auth: Auth) {
+  constructor(public auth: Auth, public router: Router) {
 
   }
 
 
   signupService(formValue: any, signupForm: any) {
+    signupForm.reset();
     this.sendingData = true;
     createUserWithEmailAndPassword(this.auth, formValue.email, formValue.password)
       .then((userCredential) => {
-        updateProfile(this.auth.currentUser!, { displayName: formValue.name })
-          .then(() => {
-            signupForm.reset();
-            this.sendingData = false;
-          })
+        this.signupSuccessfully = true;
+        this.directToLogin(formValue.name);
       })
       .catch((error) => {
         console.log(error.message);
       })
   }
 
+
+  directToLogin(formValueName: any) {
+    updateProfile(this.auth.currentUser!, { displayName: formValueName })
+      .then(() => {
+        this.sendingData = false;
+        this.router.navigateByUrl('/login');
+      })
+  }
 }
