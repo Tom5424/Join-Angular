206d1ff3a0adc295eee9797f8728238d5f328b0d
Tom <tom.petri7@gmail.com>
2023-12-14 01:15:24 +0100

    The Form can now be cleared and some values from the Fields can be read out

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,17 +13,23 @@ export class AddTaskComponent {
   dropdownMenuAssignedToIsOpen: boolean = false;
   dropdownMenuCategoryIsOpen: boolean = false;
   contactIsSelected: boolean = false;
-  currentSelectedPrio: string = '';
+  activePrio: string = '';
+  selectedCategory: string = '';
   priorities: string[] = ['urgent', 'medium', 'low'];
+
+
+  addTaskForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    assignedTo: new FormControl(''),
+    dueDate: new FormControl(''),
+    prio: new FormControl(''),
+    category: new FormControl(''),
+  });
 
 
   openDropdownMenuAssignedTo() {
     this.dropdownMenuAssignedToIsOpen = !this.dropdownMenuAssignedToIsOpen;
-  }
-
-
-  selectContact() {
-    this.contactIsSelected = !this.contactIsSelected;
   }
 
 
@@ -31,17 +38,45 @@ export class AddTaskComponent {
   }
 
 
+  closeDropdownIfClickOutSide() {
+    this.dropdownMenuAssignedToIsOpen = false;
+    this.dropdownMenuCategoryIsOpen = false;
+  }
+
+
+  selectContact() {
+    this.contactIsSelected = !this.contactIsSelected;
+  }
+
+
   selectPrio(selectedPrio: string) {
     this.priorities.forEach((currentPrio) => {
       if (selectedPrio == currentPrio) {
-        this.currentSelectedPrio = currentPrio;
+        this.activePrio = currentPrio;
       }
     });
   }
 
 
-  closeDropdownIfClickOutSide() {
-    this.dropdownMenuAssignedToIsOpen = false;
+  selectCategory(text: string) { // rename Parameter text
+    this.selectedCategory = text;
     this.dropdownMenuCategoryIsOpen = false;
+  }
+
+
+  onSubmitForm() {
+    this.addTaskForm.patchValue({
+      title: this.addTaskForm.controls.title.value,
+      description: this.addTaskForm.controls.description.value,
+      dueDate: this.addTaskForm.controls.dueDate.value,
+      category: this.selectedCategory,
+    });
+  }
+
+
+  clearForm() {
+    this.addTaskForm.reset();
+    this.activePrio = '';
+    this.selectedCategory = '';
   }
 }
