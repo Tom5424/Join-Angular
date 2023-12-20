c2f3ce966ae60ed45af46d5d70e5e4cfc4676925
Tom <tom.petri7@gmail.com>
2023-12-20 17:56:43 +0100

    Added Mini View from Task Card (for now Hardcoded)

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -9,5 +9,15 @@ import { Component } from '@angular/core';
 
 
 export class BoardComponent {
+  @ViewChild('inputFindTask') inputFindTask!: ElementRef;
 
+
+  constructor(public renderer: Renderer2) {
+
+  }
+
+
+  focusInput() {
+    this.renderer.selectRootElement(this.inputFindTask.nativeElement).focus();
+  }
 }
