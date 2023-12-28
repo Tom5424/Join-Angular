ff5920519d39821cb271f1fa0f5aeab24b67668a
Tom <tom.petri7@gmail.com>
2023-12-28 22:19:52 +0100

    Now retrieves all Input from the Add Task form and displays Feedback to the User when the Task is successfully create

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -14,18 +14,18 @@ import { CreateNewContactService } from 'src/app/services/create-new-contact.ser
 export class AddTaskComponent implements OnInit {
   dropdownMenuAssignedToIsOpen: boolean = false;
   dropdownMenuCategoryIsOpen: boolean = false;
-  // contactIsSelected: boolean = false;
-  selectedContact: any = '';
   activePrioBtn: string = '';
   selectedCategory: string = '';
   priorities: string[] = ['urgent', 'medium', 'low'];
   selectedContacts: Array<Contact> = [];
 
+  //// Still has to move to create task service
+  taskCreated: boolean = false;
 
   addTaskForm = new FormGroup({
     title: new FormControl('', [Validators.required, Validators.maxLength(10)]),
     description: new FormControl('', Validators.maxLength(150)),
-    assignedTo: new FormControl(''),
+    contacts: new FormControl(this.selectedContacts),
     dueDate: new FormControl('', Validators.required),
     prio: new FormControl(''),
     category: new FormControl(''),
@@ -88,22 +88,23 @@ export class AddTaskComponent implements OnInit {
   }
 
 
-  addTask() {
+  createTask() {
     this.addTaskForm.patchValue({
       title: this.addTaskForm.controls.title.value,
       description: this.addTaskForm.controls.description.value,
-      assignedTo: this.selectedContact,
+      contacts: this.selectedContacts,
       dueDate: this.addTaskForm.controls.dueDate.value,
       prio: this.activePrioBtn,
       category: this.selectedCategory,
     });
+    this.taskCreated = true;
     this.clearForm();
   }
 
 
   clearForm() {
     this.addTaskForm.reset();
-    this.selectedContact = '';
+    this.selectedContacts = [];
     this.activePrioBtn = '';
     this.selectedCategory = '';
   }
