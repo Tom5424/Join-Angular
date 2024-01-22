227f2a07b3fb93622310e31e4996107b340113b0
Tom <tom.petri7@gmail.com>
2024-01-22 19:23:10 +0100

    Made the Intro Component responsive

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss', './intro.component.media.scss']
 })
 
 
