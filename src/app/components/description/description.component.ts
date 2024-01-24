2ea1f9759cfe50567cbd52eb7775e2b835b16fae
Tom <tom.petri7@gmail.com>
2024-01-24 14:07:24 +0100

    Made the Description Component responsive

--- src/app/components/description/description.component.ts
+++ src/app/components/description/description.component.ts
@@ -4,10 +4,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
-  styleUrls: ['./description.component.scss']
+  styleUrls: ['./description.component.scss', './description.component.media.scss']
 })
 
 
 export class DescriptionComponent {
 
 }
+
